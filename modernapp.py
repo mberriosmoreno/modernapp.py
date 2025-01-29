0f32746496b8dce3585f0d2281ac5b93a62be40c
mberriosmoreno <94083004+mberriosmoreno@users.noreply.github.com>
2025-01-29 17:56:56 -0600

    Create modernapp.py

---
+++ modernapp.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="Admin Panel", page_icon="⚙️", layout="wide")
+
+st.sidebar.success("Selecciona una página arriba.")  # Mensaje en la barra lateral
+st.title("Bienvenido a la Plataforma de Administración")
+st.write("Usa el menú de la izquierda para navegar entre las páginas.")
