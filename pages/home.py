ad2460ae5a0839a422c6b68d7f71a5ffc48e66a6
mberriosmoreno <94083004+mberriosmoreno@users.noreply.github.com>
2025-01-29 19:15:27 -0600

    Update home.py

--- pages/home.py
+++ pages/home.py
@@ -5,4 +5,4 @@ st.set_page_config(page_title="Inicio", page_icon="🏠", layout="wide")
 st.title("🏠 Página de Inicio")
 st.write("Bienvenido al sistema de administración.")
 
-st.image("https://github.com/mberriosmoreno/modernapp.py/blob/main/assets/streamlit-logo-secondary-colormark-darktext.png", width=600)  # Imagen de bienvenida
+st.image("assets/streamlit-logo-secondary-colormark-darktext.png", width=600)  # Imagen de bienvenida
